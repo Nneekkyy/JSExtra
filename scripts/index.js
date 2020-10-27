a6c090e651491b5eb601c14c91e09aea957e8e6d
Alexey Krasnov <neky@list.ru>
2020-10-27 15:04:33 +0500

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,24 @@
 const title = document.querySelector(".header__rectangle");
-
+const chat = document.querySelector(".chat__field");
 
 function scrollHandler(event) {
-  // window - это тоже объект, который у нас есть всегда и у него тоже полно значений
-  console.log(window.scrollY);
-  if (window.scrollY > 500) {
-    title.classList.add(".header__top-fixed")
-  } else {
-    title.classList.remove(".header__top-fixed")
+  if (window.scrollY > 300 && window.scrollY < 1943) {
+    title.classList.add("header__top-fixed");
+  }
+
+  else {
+    title.classList.remove("header__top-fixed");
   }
 }
 
+function chatUnhidden() {
+  chat.classList.add("chat__field_unhidden");
+}
+// function chatHidden(event) {
+//   if (event.target.class != "chat__field") {
+//     chat.classList.remove("chat__field_unhidden");
+//   }
+// }
 document.addEventListener('scroll', scrollHandler);
-
-// а это пример того, что использовать addEventListener можно много раз к одному элементу
-
-//document.addEventListener('scroll', popupClick)
+document.addEventListener('click', chatUnhidden);
+document.addEventListener('click', chatHidden);
