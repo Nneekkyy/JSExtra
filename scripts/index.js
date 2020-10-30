c07740156169f73b9e8fb82f67758d1abc5e1514
Alexey Krasnov <neky@list.ru>
2020-10-30 09:59:45 +0500

    решена первая задача

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,26 @@ const chatField = document.querySelector(".chat__field");
 const chat = document.querySelector(".chat");
 const button = document.querySelector(".chat__button");
 
+let maxHeight = title.offsetTop;
 
+function scrollHandler() {
 
-function scrollHandler(event) {
-  if (window.scrollY > 300 && window.scrollY < 1943) {
-    title.classList.add("header__top-fixed");
+  if (window.scrollY >= maxHeight && window.scrollY <= 1950) {
+    title.classList.add('header__top-fixed');
+  } else {
+    title.classList.remove('header__top-fixed');
   }
-
-  else {
-    title.classList.remove("header__top-fixed");
-  }
 }
 
-function showChat() {
-  chatField.classList.add('chat__field_unhidden');
+window.addEventListener('scroll', scrollHandler)
+
+
+function scrolling(e) {
+
+  console.log(window.scrollY);
 }
 
-function closeChat() {
-  chatField.classList.remove('chat__field_unhidden')
-}
-
-
-
-
-document.addEventListener('scroll', scrollHandler);
+document.addEventListener('scroll', scrolling);
 
 
 
@@ -37,18 +33,9 @@ document.addEventListener('scroll', scrollHandler);
 
 
 
-
-
-
-
-
-
-
-
-
-function popupClick(event) {
-   console.log('event.target:', event.target.className);
-   console.log('event.target:', event.target.classList);
-   console.log('event.currentTarget:', event.currentTarget);
-}
-document.addEventListener('click', popupClick);
+// function popupClick(event) {
+//    console.log('event.target:', event.target.className);
+//    console.log('event.target:', event.target.classList);
+//    console.log('event.currentTarget:', event.currentTarget);
+// }
+// document.addEventListener('click', popupClick);
